651847516d7edce925e864bf48828d6560d310c9
RiaRiver <riria.river@gmail.com>
2021-07-18 23:23:09 +0300

    fix gcd-game to work with negative numbers

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -5,14 +5,16 @@ const description = 'Find the greatest common divisor of given numbers.';
 
 export const getGcd = (num1, num2) => {
   if (num1 === 0 && num2 === 0) return NaN;
-  if (num2 === 0) return num1;
+  if (num2 === 0) return Math.abs(num1);
 
   return getGcd(num2, num1 % num2);
 };
 
 const generateRoundData = (rangeStart = 0, rangeEnd = 150) => {
   const firstNumber = getRandomIntInclusive(rangeStart, rangeEnd);
-  const secondNumber = getRandomIntInclusive(rangeStart + 1, rangeEnd);
+  const secondNumber = getRandomIntInclusive(rangeStart, rangeEnd);
+
+  if (firstNumber === 0 && secondNumber === 0) return generateRoundData(rangeStart, rangeEnd);
 
   const question = `${firstNumber} ${secondNumber}`;
   const answer = getGcd(firstNumber, secondNumber).toString();
